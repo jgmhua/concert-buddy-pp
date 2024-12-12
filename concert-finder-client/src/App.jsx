f49231ab2688c8a3da13e9874ab76ff6e25eb6e1
Jessica Hua <jhua@live.ca>
2024-12-12 14:10:58 -0500

    edit login routes.

--- concert-finder-client/src/App.jsx
+++ concert-finder-client/src/App.jsx
@@ -1,19 +1,25 @@
 import "./App.scss";
 import axios from "axios";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
+const { VITE_BASE_URL, VITE_PORT } = import.meta.env;
+
 export default function App() {
+	
+
 	return (
 		<BrowserRouter>
 			<article className="app">
 				<Header />
 				<Routes>
-					<Route path="/" element={<HomePage />} />
+					{/* <Route path="/" element={<HomePage />} /> */}
+					<Route path="/" element={<LoginPage />} />
 					<Route path="/home" element={<Navigate to="/" />} />
 					<Route path="/notfound" element={<NotFoundPage />} />
 					<Route path="*" element={<Navigate to="/notfound" />} />
--- concert-finder-server/routes/loggedRoutes.js
+++ concert-finder-server/routes/loggedRoutes.js
@@ -4,7 +4,30 @@ import express from "express";
 import querystring from "querystring";
 const router = express.Router();
 
-const { CLIENT_ID, REDIRECT_URL, CLIENT_SECRET  } = process.env;
+const { CLIENT_ID, REDIRECT_URL, CLIENT_SECRET, BASE_URL, PORT } = process.env;
+
+//helper functions?
+async function getUserData(credentials) {
+  const { access_token, refresh_token, token_type, scope, expires_in } = credentials;
+  console.log(credentials,"credentials");
+
+  try {
+    const userData = await axios.get('https://api.spotify.com/v1/me',
+        {
+            headers: {
+                'Authorization': 'Bearer ' + {access_token}
+              },
+        }  
+    );
+    console.log(userData.data, "user's profile data");
+    res.status(200).send(userData.data);
+  } catch (error) {
+    console.error('Error fetching profile');
+    res.status(500).send('Failed to fetch profile.');
+  }
+
+}
+
 
 router.get('/', async (req, res) => {
 let code = req.query.code || null;
@@ -29,8 +52,10 @@ let code = req.query.code || null;
         }  
     );
 
-    const { access_token, refresh_token, token_type, scope, expires_in } = response.data;
-    res.send({ access_token, refresh_token, token_type, scope, expires_in })
+    getUserData(response.data);
+
+    // res.send({ access_token, refresh_token, token_type, scope, expires_in })
+
   } catch (error) {
     console.error('Error fetching token');
     res.status(500).send('Failed to exchange code for tokens.');
