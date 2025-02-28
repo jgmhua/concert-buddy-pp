1cbb608b01de1152b93d4cd9b8545c5f15b89c8f
Jessica Hua <jhua@live.ca>
2025-02-28 17:59:05 -0500

    fix modal popup, add email list to form.

--- concert-finder-client/src/components/ConcertCard/ConcertCard.jsx
+++ concert-finder-client/src/components/ConcertCard/ConcertCard.jsx
@@ -1,10 +1,14 @@
+import axios from "axios";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import Button from "../Button/Button";
 import "./ConcertCard.scss";
-import { useState } from "react";
+
+const {VITE_BASE_URL,VITE_PORT} = import.meta.env;
 
 //FIXME: figure out what to do with events that don't have an external url
 //TODO: work on openModal function
+/* TODO: for now, since we don't have access to their emails through Spotify, users will have to manually type in each person's email -- think about solutions for future*/
 
 export default function ConcertCard({
 	showConcertDetails,
@@ -12,26 +16,49 @@ export default function ConcertCard({
 	handleEventClick,
 }) {
 	const [showModal, setShowModal] = useState(false);
+	const [allEmails, setAllEmails] = useState([]);
+	const [emailList, setEmailList] = useState("");
+	// const allEmails = [];
 
-	function openModal() {
-		setShowModal(true);
-	}
+	const handleFriendInvites = async (event, allEmails) => {
+		event.preventDefault();
+		console.log("emailList:", emailList);
+		setAllEmails(emailList.split(","));
+		
+		//2025-02-28
+		//TODO: come back to this and continue
+		//TODO: pick an integration option for sending emails https://react.email/docs/introduction and then set up emailing functionality
+		// try {
+		// 	const response = await axios.post(
+		// 		`${VITE_BASE_URL}:${VITE_PORT}/invite`,
+		// } catch (error) {
+		// }
+
+	};
 
 	return (
-		<li className="concert" onClick={() => handleEventClick(event.id)}>
+		<li className="concert">
 			<div className="concert__main">
-				<div className="concert__image-div">
-					<img className="concert__image" src={event.images} />
+				<div className="concert__left">
+					<div className="concert__image-div">
+						<img className="concert__image" src={event.images} />
+					</div>
+					<h4 className="concert__name">{event.name}</h4>
 				</div>
-				<h4 className="concert__name">{event.name}</h4>
+				<span
+					onClick={() => handleEventClick(event.id)}
+					className="material-symbols-outlined material-symbols-outlined--circle-down"
+				>
+					expand_circle_down
+				</span>
 			</div>
-			<span
+			{/* <span
 				className={`concert hover-text ${
 					showConcertDetails !== event.id ? "hover-text--hover" : ""
 				}`}
 			>
 				See concert details
-			</span>
+			</span> */}
 			{showConcertDetails === event.id ? (
 				<article className="concert__popup">
 					<section className="concert__info">
@@ -68,36 +95,64 @@ export default function ConcertCard({
 						</Link>
 						<span
 							className="material-symbols-outlined concert-btn"
-							onClick={() => openModal()}
+							onClick={() => setShowModal(true)}
 						>
 							group_add
 						</span>
-					</div>
-					{showModal === true ? (
-						<section className="modal">
-							{/* <div className="modal__overlay"> */}
+						{showModal && (
+							<section className="modal">
 								<div className="modal__content">
+									<span
+										className="material-symbols-outlined modal__close"
+										onClick={() => setShowModal(false)}
+									>
+										close
+									</span>
 									<h3 className="modal__title">Invite Friends to Concert</h3>
-									<div className="modal__message">
+									<div className="modal__content">
 										{/* TODO: include some sort of image */}
-										<h4>Concert Name</h4>
-										<h4>Date</h4>
-										<h4>Location</h4>
-										<h4>Link</h4>
-										<form className="invite">
-										<label>
-											Invite Friends
-											{/* TODO: for now, since we don't have access to their emails through Spotify, users will have to manually type in each person's email -- think about solutions for future*/}
-											<input></input>
-										</label>
+										<div className="modal__concert-details">
+											<h4>Concert Name</h4>
+											<h4>Date</h4>
+											<h4>Location</h4>
+											<h4>Link</h4>
+										</div>
+										<form
+											className="invite__form"
+											onSubmit={handleFriendInvites}
+										>
+											<label className="invite__label">
+												Invite Friends
+												<small className="message message--small">
+													Enter emails separated by comma.
+												</small>
+												<input
+													className="invite__input"
+													name="emails"
+													type="email"
+													multiple
+													placeholder="Ex: friend1@email.com,friend2@email.com"
+													onChange={(e) => {
+														setEmailList(e.target.value);
+													}}
+												/>
+											</label>
+											<div>
+												<h4>You're sending invites to the following friends</h4>
+												{allEmails &&
+													allEmails.map((email) => {
+														return <p key={email}>{email}</p>;
+													})}
+											</div>
+											<button type="submit" className="invite__submit">
+												Submit
+											</button>
 										</form>
 									</div>
 								</div>
-							{/* </div> */}
-						</section>
-					) : (
-						""
-					)}
+							</section>
+						)}
+					</div>
 				</article>
 			) : (
 				""
